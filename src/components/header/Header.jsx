c0365c8835e0d73b524f05dbdfa902b052b1c41b
RonnyKn <ronnykurniawn@gmail.com>
2023-02-12 16:14:49 +0700

    adding header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,36 @@
 import "./Header.css"
 import React from "react"
+import { Link } from "react-router-dom"
+import { SlHandbag } from "react-icons/sl"
+import userIcon from "../../assets/images/user-icon.png"
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="header">
+      <div className="container header-container">
+        <div className="header-logo">
+          <h5>Rons Furniture</h5>
+        </div>
+        <div className="header-nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="header-navicons">
+          <SlHandbag size="1.8em" />
+          <img src={userIcon} alt={`userIcon.png`} />
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "remixicon/fonts/remixicon.css"
-import "bootstrap/dist/css/bootstrap.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 
