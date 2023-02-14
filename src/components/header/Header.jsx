e1ea52423983dfba46c0f38da43e06520e1cb7a2
RonnyKn <ronnykurniawn@gmail.com>
2023-02-14 12:33:20 +0700

    fix active class on navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,10 @@
 import "./Header.css"
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { SlHandbag } from "react-icons/sl"
 import userIcon from "../../assets/images/user-icon.png"
-import { useState } from "react"
 
 const Header = () => {
-  const [activeNav, setActiveNav] = useState("/")
   const navigations = [
     {
       navbar: "Home",
@@ -31,12 +29,15 @@ const Header = () => {
         <div className="header-nav">
           <ul>
             {navigations.map((val, idx) => (
-              <li
-                onClick={() => setActiveNav(`${val?.to}`)}
-                className={activeNav === val?.to && "nav-active"}
-                key={idx}
-              >
-                <Link to={val?.to}>{val?.navbar}</Link>
+              <li key={idx}>
+                <NavLink
+                  to={val?.to}
+                  className={(navClass) =>
+                    navClass.isActive ? "nav-active" : null
+                  }
+                >
+                  {val?.navbar}
+                </NavLink>
               </li>
             ))}
           </ul>
