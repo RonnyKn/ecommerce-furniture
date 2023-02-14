f1691ed4ce263ba81d5ca2038321fcdf4a8cef99
RonnyKn <ronnykurniawn@gmail.com>
2023-02-14 13:14:21 +0700

    adding hero

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,32 @@
 import "./Home.css"
 import React from "react"
+import HeroImg from "../../assets/images/hero-img.png"
 
 const Home = () => {
-  return <div>Home</div>
+  const date = new Date()
+
+  return (
+    <section className="home">
+      <div className=" home-hero">
+        <div className="home-hero-container container">
+          <div className="hero-left">
+            <p>
+              <strong>Trending Product in </strong>
+            </p>
+            <h1>Make Your Interior More Minimalistic & Modern</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat a
+              aut iure rem, aspernatur esse!
+            </p>
+            <button>SHOP NOW!</button>
+          </div>
+          <div className="hero-right">
+            <img src={HeroImg} alt={`Hero.png`} />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Home
