4347a05edcab4f8dd7de1ff6b5f4f5d6b61b898c
RonnyKn <ronnykurniawn@gmail.com>
2023-02-28 23:26:12 +0700

    adding check out

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -10,6 +10,8 @@ import { useDispatch, useSelector } from "react-redux"
 import CartCount from "../../components/cart/CartCount"
 import CartItems from "../../components/cart/CartItems"
 import CartEmpty from "../../components/cart/CartEmpty"
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion"
 
 const Cart = () => {
   const dispatch = useDispatch()
@@ -36,11 +38,18 @@ const Cart = () => {
               <div className="cartitems">
                 <CartItems cartItems={dataCartItems} />
               </div>
-              <div className="subtotal">
-                <h3>
-                  SUBTOTAL : $<span>{dataSubtotal}</span>
-                </h3>
-              </div>
+              <Link to="/checkout">
+                <motion.div
+                  className="subtotal"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  <h5>Checkout now!</h5>
+                  <h3>
+                    SUBTOTAL : $<span>{dataSubtotal}</span>
+                  </h3>
+                </motion.div>
+              </Link>
             </>
           )}
         </div>
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,8 +1,59 @@
 import "./Checkout.css"
 import React from "react"
+import CartItems from "../../components/cart/CartItems"
+import { useSelector } from "react-redux"
+import { selectCartItems } from "../../redux/slice/cartSlice"
+import { toast } from "react-toastify"
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
+import CartEmpty from "../../components/cart/CartEmpty"
 
 const Checkout = () => {
-  return <div>Checkout</div>
+  const dataCartItems = useSelector(selectCartItems)
+  return (
+    <section className="checkout">
+      <div className="container checkout-container">
+        <div className="checkout-left">
+          <form>
+            <p>Fill your data to proced Shipping</p>
+            <label>Full Name</label>
+            <input type="text" />
+            <label>Adress</label>
+            <textarea />
+            <label>Phone</label>
+            <input type="number" />
+            <Link to="/home">
+              <motion.button
+                type="button"
+                className="btn"
+                onClick={() => toast.success("Order Process Succesfully")}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Order Now!
+              </motion.button>
+            </Link>
+            <Link to="/cart">
+              <motion.button
+                className="btn"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Back to Cart
+              </motion.button>
+            </Link>
+          </form>
+        </div>
+        <div className="checkout-right">
+          {dataCartItems.length === 0 ? (
+            <CartEmpty />
+          ) : (
+            <CartItems cartItems={dataCartItems} />
+          )}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Checkout
