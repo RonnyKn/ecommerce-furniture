b36744e3bc83b741d6a92e5ae54791c3d9a2df70
RonnyKn <ronnykurniawn@gmail.com>
2023-03-03 16:14:26 +0700

    fix form submit

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -4,38 +4,48 @@ import { useSelector } from "react-redux"
 import { selectCartItems, selectTotalAmount } from "../../redux/slice/cartSlice"
 import { toast } from "react-toastify"
 import { motion } from "framer-motion"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import CartEmpty from "../../components/cart/CartEmpty"
 import CheckOutItems from "../../components/checkOutItems/CheckOutItems"
 
 const Checkout = () => {
   const dataCartItems = useSelector(selectCartItems)
   const dataSubtotal = useSelector(selectTotalAmount)
+  const navigate = useNavigate()
+
+  function handleSubmit() {
+    navigate("/home")
+  }
 
   return (
     <section className="checkout">
       <h3 className="checkout-title container">Check Out</h3>
       <div className="container checkout-container">
         <div className="checkout-left">
-          <form>
+          <form
+            onSubmit={() => {
+              handleSubmit()
+              toast.success("Order Process Succesfully")
+            }}
+          >
             <em>Fill your data to process Shipping</em>
             <label>Full Name</label>
-            <input type="text" />
+            <input type="text" required />
             <label>Address</label>
-            <textarea />
+            <textarea required />
             <label>Phone</label>
-            <input type="number" />
-            <Link to="/home">
-              <motion.button
-                type="button"
-                className="btn"
-                onClick={() => toast.success("Order Process Succesfully")}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                Order Now!
-              </motion.button>
-            </Link>
+            <input type="number" required />
+            {/* <Link to="/home"> */}
+            <motion.button
+              type="submit"
+              className="btn"
+              // onClick={() => toast.success("Order Process Succesfully")}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Order Now!
+            </motion.button>
+            {/* </Link> */}
             <Link to="/cart">
               <motion.button
                 className="btn"
