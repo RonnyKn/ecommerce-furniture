1c358936d444a5787dda340e57deb3068a83d986
RonnyKn <ronnykurniawn@gmail.com>
2023-03-07 17:58:15 +0700

    fix login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,34 @@
 import "./Login.css"
 import React from "react"
+import { motion } from "framer-motion"
 
 const Login = () => {
   return (
     <section className="login">
       <div className="container login-container">
         <h1>Login</h1>
-        <div>
-          <label>Email</label>
-          <input type="email" />
-        </div>
-        <div>
-          <label>Password</label>
-          <input type="password" />
-        </div>
+        <form action="submit">
+          <div>
+            <label>Email</label>
+            <input type="email" placeholder="Enter your email.." required />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password.."
+              required
+            />
+          </div>
+          <motion.button
+            type="submit"
+            className="btn"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Login
+          </motion.button>
+        </form>
       </div>
     </section>
   )
