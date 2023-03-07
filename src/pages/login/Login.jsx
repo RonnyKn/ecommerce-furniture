eb0d61922e4f5f9bd6ab79323054fef2b2994403
RonnyKn <ronnykurniawn@gmail.com>
2023-03-07 17:53:01 +0700

    adding login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,21 @@ import "./Login.css"
 import React from "react"
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <section className="login">
+      <div className="container login-container">
+        <h1>Login</h1>
+        <div>
+          <label>Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Login
