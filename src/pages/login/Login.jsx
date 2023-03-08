67fa211d2e06f1b5721afa7c27f295895b083daf
RonnyKn <ronnykurniawn@gmail.com>
2023-03-08 12:49:36 +0700

    adding signup and Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,39 @@
 import "./Login.css"
-import React from "react"
+import React, { useState } from "react"
 import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <section className="login">
       <div className="container login-container">
         <h1>Login</h1>
         <form action="submit">
           <div>
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email.." required />
+            <label>
+              <h4>E-mail</h4>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email.."
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
-            <label>Password</label>
+            <label>
+              <h4>Password</h4>
+            </label>
             <input
               type="password"
               placeholder="Enter your password.."
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <motion.button
@@ -28,6 +44,12 @@ const Login = () => {
           >
             Login
           </motion.button>
+          <p>
+            Dont have an Account?
+            <span>
+              <Link to="/signup"> Register here! </Link>
+            </span>
+          </p>
         </form>
       </div>
     </section>
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,8 +1,76 @@
 import "./Signup.css"
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion"
 
 const Signup = () => {
-  return <div>Signup</div>
+  const [email, setEmail] = useState("")
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [file, setFile] = useState(null)
+
+  return (
+    <section className="login">
+      <div className="container login-container">
+        <h1>Login</h1>
+        <form action="submit">
+          <div>
+            <label>
+              <h4>User Name</h4>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your User Name.."
+              required
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>
+              <h4>E-mail</h4>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email.."
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>
+              <h4>Password</h4>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your password.."
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          </div>
+          <motion.button
+            type="submit"
+            className="btn"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Sign Up
+          </motion.button>
+          <p>
+            Already have an Account?
+            <span>
+              <Link to="/login"> Login here! </Link>
+            </span>
+          </p>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Signup
