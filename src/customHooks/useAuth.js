852493e38e971824a81cf8cc68e2914a15ccbfc5
RonnyKn <ronnykurniawn@gmail.com>
2023-03-09 22:19:27 +0700

    adding custom hooks useAuth for manage user

---
+++ src/customHooks/useAuth.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../firebaseConfig/firebaseConfig"
+
+const useAuth = () => {
+  const [currentUser, setCurrentUser] = useState({})
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user)
+      } else {
+        setCurrentUser(null)
+      }
+    })
+  })
+
+  return currentUser
+}
+
+export default useAuth
