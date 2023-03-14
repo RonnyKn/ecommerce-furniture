139b9b12e880b4adf448317bda3b3d0da170e550
RonnyKn <ronnykurniawn@gmail.com>
2023-03-14 15:19:15 +0700

    styling dashboard

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,23 +1,44 @@
+import "./Admin.css"
 import React from "react"
 import { Link } from "react-router-dom"
-import "./Admin.css"
-
+import { motion } from "framer-motion"
 const Dashboard = () => {
   return (
     <section className="dashboard">
       <h3 className="container">
-        Welcome ,<span>Admin</span>
+        Welcome <span> Admin</span>
       </h3>
       <div className="container dashboard-container">
-        <div className="dashboard-menu">
-          <Link to="/dashboard/add-products">Add product</Link>
-        </div>
-        <div className="dashboard-menu">
-          <Link to="/dashboard/all-products">All product</Link>
-        </div>
-        <div className="dashboard-menu">
-          <Link to="/dashboard/users">Users</Link>
-        </div>
+        <Link to="/dashboard/add-products">
+          <motion.div
+            className="dashboard-menu"
+            style={{ background: "#e2f2b2" }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Add product
+          </motion.div>
+        </Link>
+        <Link to="/dashboard/all-products">
+          <motion.div
+            className="dashboard-menu"
+            style={{ background: "#ceebe9" }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            All product
+          </motion.div>
+        </Link>
+        <Link to="/dashboard/users">
+          <motion.div
+            className="dashboard-menu"
+            style={{ background: "#fdefe6" }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Users
+          </motion.div>
+        </Link>
       </div>
     </section>
   )
---
+++ src/admin/Users.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Users = () => {
+  return <section>Users</section>
+}
+
+export default Users
