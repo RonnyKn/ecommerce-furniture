b2584c873041e836529012d8c78504f23f5f3b30
RonnyKn <ronnykurniawn@gmail.com>
2023-03-27 23:11:16 +0700

    fix footer when login/signup

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import AdminNav from "../../admin/AdminNav"
 import Routers from "../../routers/Routers"
@@ -8,13 +8,33 @@ import Header from "../header/Header"
 const Layout = () => {
   const location = useLocation()
 
+  const footerNav = () => {
+    if (location.pathname.startsWith("/dashboard")) {
+      return null
+    }
+    if (location.pathname.startsWith("/login")) {
+      return null
+    }
+    if (location.pathname.startsWith("/signup")) {
+      return null
+    } else {
+      return <Footer />
+    }
+  }
+
+  // useEffect(() => {
+  //   footerNav()
+  // }, [])
   return (
     <>
       {location.pathname.startsWith("/dashboard") ? <AdminNav /> : <Header />}
       <div>
         <Routers />
       </div>
-      {location.pathname.startsWith("/dashboard") ? null : <Footer />}
+      {footerNav()}
+      {/* {location.pathname.startsWith("/dashboard") ? null : <Footer />} */}
+      {/* {location.pathname.startsWith("/login") ? null : <Footer />} */}
+      {/* {location.pathname.startsWith("/signup") ? null : <Footer />} */}
     </>
   )
 }
