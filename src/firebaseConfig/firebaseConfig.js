a423e4b8915506716b628b43ad18d8834643a8c2
RonnyKn <ronnykurniawn@gmail.com>
2023-03-31 13:50:26 +0700

    fix bug

--- src/firebaseConfig/firebaseConfig.js
+++ src/firebaseConfig/firebaseConfig.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsd810PbrFuNA6u1RVljxkii8Hgmluhsc",
+  apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
