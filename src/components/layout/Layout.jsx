ce94b363d580619edafd0baea8678566392e58fc
RonnyKn <ronnykurniawn@gmail.com>
2023-03-31 13:44:41 +0700

    fix bug

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { useLocation } from "react-router-dom"
 import AdminNav from "../../admin/AdminNav"
 import Routers from "../../routers/Routers"
